0730c67e210af5490db9af0bb47a662a303884ba
mabessir <mabessir@student.42.fr>
2018-04-01 22:24:42 +0200

    FINISHED

--- config/bdd.php
+++ config/bdd.php
@@ -1,5 +1,5 @@
 <?php
-$bdd = mysqli_connect(null, 'root', 'bdroot', 'shoop', 0, '/Users/mabessir/goinfre/mamp/mysql/tmp/mysql.sock');
+$bdd = mysqli_connect(null, 'root', 'bdroot', 'shoop', 0, '/Users/vguillem/goinfre/mamp/mysql/tmp/mysql.sock');
 if(!$bdd)
 die("connection failed: " . mysqli_connect_error());
 ?>
